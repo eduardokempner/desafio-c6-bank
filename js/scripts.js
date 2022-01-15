a3683ab4eaf4b416e4c8e4715674076184911a5b
Eduardo Kempner <eduardokmpr@gmail.com>
2022-01-14 23:04:27 -0300

    tabs css+js

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,24 @@ function openDropdown() {
 
 menu.addEventListener("mouseover", openDropdown);
 dropdown.addEventListener("mouseleave", openDropdown);
+
+// Tabs
+
+const navTab = document.querySelectorAll(".tab");
+const tabContent = document.querySelectorAll(".banco-completo__card");
+
+navTab.forEach((nav, index) => {
+  nav.addEventListener("click", () => {
+    navTab.forEach((itemNav) => {
+      itemNav.classList.remove("tab--active");
+    });
+
+    nav.classList.add("tab--active");
+
+    tabContent.forEach((tab) => {
+      tab.classList.remove("banco-completo__card--active");
+    });
+
+    tabContent[index].classList.add("banco-completo__card--active");
+  });
+});
